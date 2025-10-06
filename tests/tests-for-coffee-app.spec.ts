4d1069896af86c1cede5eec3d4283358979e94d0
andrewshavaryn <andrijsavarin5@gmail.com>
2025-10-06 21:27:02 +0300

    fix test #3

--- tests/tests-for-coffee-app.spec.ts
+++ tests/tests-for-coffee-app.spec.ts
@@ -62,25 +62,19 @@ test(
   async ({ page }) => {
     await page.goto("https://coffee-cart.app/");
     await page.locator('[data-test="Espresso"]').click();
-    await page.locator('[data-test="Espresso_Macchiato"]').click();
-    await page.locator('[data-test="Cappuccino"]').click();
-
+    await page.locator('[data-test="Flat_White"]').click();
+    await page.locator('[data-test="Americano"]').click();
     await expect(
       page.getByText(
         "It's your lucky day! Get an extra cup of Mocha for $4.espressochocolate"
       )
     ).toBeVisible();
-
     await page.getByRole("button", { name: "Yes, of course!" }).click();
-    await page.getByRole("link", { name: "Cart page" }).click();
-    await expect(
-      page.locator("div").filter({ hasText: "(Discounted) Mocha x 1+-" }).nth(1)
-    ).toBeVisible();
-
-    await page.getByRole("link", { name: "Menu page" }).click();
     await page.locator('[data-test="checkout"]').click();
-    await page.getByRole("textbox", { name: "Name" }).fill("abdreew");
-    await page.getByRole("textbox", { name: "Email" }).fill("test@gmail.com");
+    await page.getByRole("textbox", { name: "Name" }).fill("andrew");
+    await page.getByRole("textbox", { name: "Email" }).fill("andrew@gmail.com");
+    await page.getByRole("checkbox", { name: "Promotion checkbox" }).check();
+    await page.getByRole("button", { name: "Submit" }).click();
     await expect(
       page.getByRole("button", { name: "Thanks for your purchase." })
     ).toBeVisible();
