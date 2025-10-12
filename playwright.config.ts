cbb8e3d3f1144b8f159b084bf44b4f5f65de111f
andrewshavaryn <andrijsavarin5@gmail.com>
2025-10-12 21:36:15 +0300

    new settings 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,9 +46,10 @@ export default defineConfig({
         // testIdAttribute: "data-test",
       },
     },
+    
     {
       name: "css-selectors-and-aria-attributes",
-      testMatch: "**/css-selectors-coffee-app/*.spec.ts",
+      testMatch: "**/css-selectors-and-aria-atributes/*.spec.ts", // ← виправлено
       use: {
         ...devices["Desktop Chrome"],
         baseURL: "https://coffee-cart.app/",
