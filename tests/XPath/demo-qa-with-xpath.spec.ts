9f8da9ab866e05d53345f4d37ddd8a0e8a7a8036
andrewshavaryn <andrijsavarin5@gmail.com>
2025-10-17 15:39:25 +0300

    new test

---
+++ tests/XPath/demo-qa-with-xpath.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+test(
+  "DEMOQA-0001 User can submit the form with valid data",
+  {
+    tag: ["@smoke"],
+    annotation: {
+      type: "description",
+      description: "Positive cases for submit the form",
+    },
+  },
+
+  async ({ page }) => {
+    await page.goto("/text-box");
+
+    await expect(page.locator("//*[@id='userName']")).toBeVisible();
+    await page.locator("//*[@id='userName']").fill("Andrew");
+
+    await expect(page.locator("//*[@id='userEmail']")).toBeVisible();
+    await page.locator("//*[@id='userEmail']").fill("andrewtest@gmail.com");
+
+    await expect(page.locator("//*[@id='currentAddress']")).toBeVisible();
+    await page.locator("//*[@id='currentAddress']").fill("Paradise City");
+
+    await expect(page.locator("//*[@id='permanentAddress']")).toBeVisible();
+    await page.locator("//*[@id='permanentAddress']").fill("Unvgvar, Ukraine");
+
+    await page.locator("//*[@id='submit']").click();
+    await expect(page.locator("//div[@id='output']")).toBeVisible();
+  }
+);
