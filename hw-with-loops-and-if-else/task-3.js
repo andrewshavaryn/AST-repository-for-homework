8ded923b9efca88c0da593af4eccf5e1e8f8302e
andrewshavaryn <andrijsavarin5@gmail.com>
2025-10-31 01:25:51 +0200

    task 3 for HW

---
+++ hw-with-loops-and-if-else/task-3.js
@@ -0,0 +1,18 @@
+/*
+Перевірка оцінки
+Якщо бал >= 50 — "Тест складено".
+Якщо < 50 — "Тест не складено".
+Вхід: Бал (наприклад, 42)
+*/
+
+const studentScore = 42;
+
+if (isNaN(studentScore)) {
+  console.log("Invalid score, please enter score as a Number");
+} else if (studentScore < 0) {
+  console.log("Invalid score: should be not less than 0");
+} else if (studentScore >= 50) {
+  console.log("Тест складено");
+} else {
+  console.log("Тест не складено");
+}
