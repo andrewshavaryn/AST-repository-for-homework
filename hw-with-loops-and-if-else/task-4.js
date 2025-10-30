c546141767e987d2d019c2ddc41aff1c715ee2ac
andrewshavaryn <andrijsavarin5@gmail.com>
2025-10-31 01:44:45 +0200

    task 4 for HW

---
+++ hw-with-loops-and-if-else/task-4.js
@@ -0,0 +1,22 @@
+/*
+Task 4
+Вік для голосування
+Напишіть програму, яка перевіряє, чи можна користувачу голосувати.
+Вхід: Вік (наприклад, 17)
+Вихід:
+- Якщо >= 18: "Ви можете голосувати."
+- Інакше: "Ви ще не можете голосувати."
+*/
+
+
+const age = 17;
+
+if (isNaN(age)) {
+  console.log("Invalid age, please enter age as a Number");
+} else if (age < 0) {
+  console.log("Invalid age: should be not less than 0");
+} else if (age >= 18) {
+  console.log("Ви можете голосувати");
+} else {
+  console.log("Ви ще не можете голосувати");
+}
