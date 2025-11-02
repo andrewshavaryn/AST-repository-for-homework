6bc0ab099aa57764b1f6ce8ee538b1e0bdd8baa5
andrewshavaryn <andrijsavarin5@gmail.com>
2025-11-03 00:40:55 +0200

    new test2

---
+++ hw-with-loops-and-functions/tests-for-task2-greetings-by-time-spec.ts
@@ -0,0 +1,8 @@
+import test, { expect } from "@playwright/test";
+import { sayHiByHour } from "./task-2-greetings-by-time";
+
+test("task-2: Граничне значення для доброго ранку", async () => {
+  const result = sayHiByHour(11);
+  expect(result).toBe("Доброго ранку");
+  console.log(result);
+});
