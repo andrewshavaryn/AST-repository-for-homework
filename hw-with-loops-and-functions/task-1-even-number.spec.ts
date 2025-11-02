e87282ae0196e152778c2deb30c7c3261be3a9ea
andrewshavaryn <andrijsavarin5@gmail.com>
2025-11-03 00:13:35 +0200

    new test

--- hw-with-loops-and-functions/task-1-even-number.spec.ts
+++ hw-with-loops-and-functions/task-1-even-number.spec.ts
@@ -5,24 +5,29 @@ import { isEvenNumber } from './task-1-even-number';
 test("task-1: парне число", async () => {
   const result = isEvenNumber(4);
   expect(result).toBe("Число парне");
+  console.log(result);
 });
 
 test("task-1: непарне число", async () => {
   const result = isEvenNumber(3);
   expect(result).toBe("Число непарне");
+  console.log(result);
 });
 
 test("task-1: невалідне число", async () => {
   const result = isEvenNumber(NaN);
   expect(result).toBe("Будь ласка, введи коректне число!");
+  console.log(result);
 });
 
 test("task-1: дробове число", async () => {
   const result = isEvenNumber(-2.2);
   expect(result).toBe("Число непарне");
+  console.log(result);
 });
 
 test("task-1: нуль це парне число", async () => {
   const result = isEvenNumber(0);
   expect(result).toBe("Число парне");
+  console.log(result);
 });
