b05011c962e0d017dbc862d859ce7245913c3f22
andrewshavaryn <andrijsavarin5@gmail.com>
2025-11-03 00:59:35 +0200

    small fix

--- tests/unit-tests-with-functions/tests-for-task2-greetings-by-time.spec.ts
+++ tests/unit-tests-with-functions/tests-for-task2-greetings-by-time.spec.ts
@@ -3,6 +3,6 @@ import { sayHiByHour } from "../../hw-with-loops-and-functions/task-2-greetings-
 
 test("task-2: Граничне значення для доброго ранку", async () => {
   const result = sayHiByHour(11);
-  expect(result).toBe("Доброго ранку");
+  expect(result).toBe("Доброго ранку!");
   console.log(result);
 });
