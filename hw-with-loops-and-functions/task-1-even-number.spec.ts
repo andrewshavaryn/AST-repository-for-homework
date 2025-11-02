d607a1a6bc82fb198a073d263c192d0bdc55e43f
andrewshavaryn <andrijsavarin5@gmail.com>
2025-11-03 00:01:33 +0200

    new dir for tests

--- hw-with-loops-and-functions/task-1-even-number.spec.ts
+++ hw-with-loops-and-functions/task-1-even-number.spec.ts
@@ -1,36 +1,28 @@
 import test, { expect } from "@playwright/test";
 
-function checkEvenOdd(number: number) { 
-  if (isNaN(number) || typeof number !== "number") {
-    return "Будь ласка, введи коректне число!";
-  } else if (number % 2 === 0) {
-    return "Число парне";
-  } else {
-    return "Число непарне";
-  }
-}
+import { isEvenNumber } from './task-1-even-number';
 
 test("task-1: парне число", async () => {
-  const result = checkEvenOdd(4);
+  const result = isEvenNumber(4);
   expect(result).toBe("Число парне");
 });
 
 test("task-1: непарне число", async () => {
-  const result = checkEvenOdd(3);
+  const result = isEvenNumber(3);
   expect(result).toBe("Число непарне");
 });
 
 test("task-1: невалідне число", async () => {
-  const result = checkEvenOdd(NaN);
+  const result = isEvenNumber(NaN);
   expect(result).toBe("Будь ласка, введи коректне число!");
 });
 
 test("task-1: дробове число", async () => {
-  const result = checkEvenOdd(-2.2);
+  const result = isEvenNumber(-2.2);
   expect(result).toBe("Число непарне");
 });
 
 test("task-1: нуль це парне число", async () => {
-  const result = checkEvenOdd(0);
+  const result = isEvenNumber(0);
   expect(result).toBe("Число парне");
 });
