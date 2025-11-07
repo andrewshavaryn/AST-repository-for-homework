7f2291cfe3f79358c7727508b95fefc21151fd93
andrewshavaryn <andrijsavarin5@gmail.com>
2025-11-07 21:08:20 +0200

    new tests and files

--- tests/tests-with-objects/coffee-cart-with-objects-spec.ts
+++ tests/tests-with-objects/coffee-cart-with-objects-spec.ts
@@ -48,3 +48,31 @@ for (const data of testData) {
   });
 }
 
+
+function getLocators(page: Page) {
+  return {
+    drinks: (drink: string) => page.locator(`[data-test="${drink}"]`),
+    nameInput: () => page.getByRole("textbox", { name: "Name" }),
+    emailInput: () => page.getByRole("textbox", { name: "Email" }),
+    promotionCheckbox: () =>
+      page.getByRole("checkbox", { name: "Promotion checkbox" }),
+    submitButton: () => page.getByRole("button", { name: "Submit" }),
+    checkoutButton: () => page.locator('[data-test="checkout"]'),
+  };
+}
+
+test(`PS-001151 param`, async ({ page }) => {
+  const locators = getLocators(page);
+
+  await page.goto("https://coffee-cart.app/");
+
+  await locators.drinks("Espresso").click();
+  await locators.checkoutButton().click();
+
+  await locators.nameInput().fill("Pavlo");
+  await locators.emailInput().fill("Pavlo@gm.com");
+
+  await locators.promotionCheckbox().click();
+  await locators.submitButton().click();
+});
+
