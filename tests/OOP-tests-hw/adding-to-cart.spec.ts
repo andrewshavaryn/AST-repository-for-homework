462224c6d2a5d77e78a0dea7cc3a9176fe12ea82
andrewshavaryn <andrijsavarin5@gmail.com>
2025-12-01 00:34:48 +0200

    new tests

---
+++ tests/OOP-tests-hw/adding-to-cart.spec.ts
@@ -0,0 +1,15 @@
+import test, { expect } from "@playwright/test";
+import { LoginPage } from "../OOP-classes-hw/LoginPage/LoginPage";
+import { ProductsPage } from "../OOP-classes-hw/ProductPage/ProductsPage";
+
+test("add product to cart", { tag: ["@regression"] }, async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const productPage = new ProductsPage(page);
+
+  await page.goto("https://www.saucedemo.com/");
+  await loginPage.fillUsername("standard_user");
+  await loginPage.fillPassword("secret_sauce");
+  await loginPage.clickLogin();
+
+  await productPage.getProductCard("Sauce Labs Backpack").clickAddToCart();
+});
--- tests/OOP-tests-hw/login.spec.ts
+++ tests/OOP-tests-hw/login.spec.ts
@@ -17,16 +17,3 @@ test(
     await expect(loginPage.locators.passwordInputLocator).toBeVisible();
   }
 );
-
-
-test("add to cart", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const productPage = new ProductsPage(page);
-
-  await page.goto("https://www.saucedemo.com/");
-  await loginPage.fillUsername("standard_user");
-  await loginPage.fillPassword("secret_sauce");
-  await loginPage.clickLogin();
-
-  await productPage.getProductCard("Sauce Labs Backpack").clickAddToCart();
-});
