011553f1b977c3d276c52a90f4613212cfeedc01
andrewshavaryn <andrijsavarin5@gmail.com>
2025-12-08 00:40:42 +0200

    changes in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,15 +24,17 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     //baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
     video: "retain-on-failure",
     screenshot: "only-on-failure",
+    baseURL: "https://demo.learnwebdriverio.com/",
+    storageState: "storageState.json",
     actionTimeout: 30 * 1000,
   },
 
