3237ef6e316bc2f83292f4153dddf220254657c2
andrewshavaryn <andrijsavarin5@gmail.com>
2025-12-08 04:47:09 +0200

    new tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,6 @@ export default defineConfig({
     video: "retain-on-failure",
     screenshot: "only-on-failure",
     baseURL: "https://demo.learnwebdriverio.com/",
-    storageState: "storageState.json",
     actionTimeout: 30 * 1000,
     navigationTimeout: 60 * 1000,
   },
@@ -69,7 +68,7 @@ export default defineConfig({
       use: {
         ...devices["Desktop Chrome"],
         baseURL: "https://demo.learnwebdriverio.com",
-        storageState: undefined,
+        storageState: "storageState.json",
       },
     },
 
