ed90d938418d498d5bda9f418aa9cfc985cc7ecb
andrewshavaryn <andrijsavarin5@gmail.com>
2025-12-08 02:25:02 +0200

    changes in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -134,9 +134,11 @@ export default defineConfig({
     {
       name: "zara",
       testDir: "tests/zara",
+      timeout: 90 * 1000,
       use: {
         ...devices["Desktop Chrome"],
         baseURL: "https://www.zara.com/es/en/",
+        navigationTimeout: 60 * 1000,
       },
     },
 
