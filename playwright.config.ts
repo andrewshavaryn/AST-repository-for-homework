bcd2a0d161c77f802c5cf1c78ca4cd2998e59cf1
andrewshavaryn <andrijsavarin5@gmail.com>
2025-12-08 05:12:31 +0200

    changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
     video: "retain-on-failure",
     screenshot: "only-on-failure",
     baseURL: "https://demo.learnwebdriverio.com/",
+    storageState: "storageState.json",
     actionTimeout: 30 * 1000,
     navigationTimeout: 60 * 1000,
   },
